25bc64ddb9eff42f3e75e6fb9c5062b926372e29
Jacob Chvatal <jake@isnt.online>
2021-03-26 19:27:56 -0700

    add nix flake and direnv

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, ... }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages."${system}";
+      in {
+       devShell = pkgs.mkShell {
+         nativeBuildInputs = with pkgs; [
+           rust-analyzer
+           cargo
+            # zig.packages."${system}".master.latest
+          ];
+        };
+      });
+}
